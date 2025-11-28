f677e76177b666f50c089c1ab9381e9fed7fe136
GIOVANNE ISAAC MARQUES <25170154@educ123.sp.senai.br>
2025-11-28 09:58:03 -0300

    dasdsa

--- frontend/src/app/ft/treinamento/[id]/page.jsx
+++ frontend/src/app/ft/treinamento/[id]/page.jsx
@@ -1,16 +1,14 @@
-"use client";
+'use client';
 
 /*
   Página onde o FT ou ADM acessa determinado treinamento e pode realizar alterações nele.
   Nesta página podemos trocar o nome, descrição e status dos treinamentos 
 */
 
-import './treinamentoID.css'
+import { useState, useEffect } from 'react';
+import { useParams } from 'next/navigation';
 
-import { useState, useEffect } from "react";
-import { useParams } from "next/navigation";
-
-import LogoGM from "@/components/LogoGM";
+import LogoGM from '@/components/LogoGM';
 import EstadoTreinamento from '@/components/EstadoTreinamento/page';
 import Sessoes from '@/components/ft/sessoes';
 
@@ -38,7 +36,7 @@ export default function VerTreinamento() {
                     console.log(data.mensagem);
                 }
             } catch (err) {
-                console.error("Erro ao carregar treinamento:", err);
+                console.error('Erro ao carregar treinamento:', err);
             }
 
         }
@@ -79,7 +77,7 @@ export default function VerTreinamento() {
                 console.log(data.mensagem);
             }
         } catch (err) {
-            console.error("Erro ao carregar sessões:", err);
+            console.error('Erro ao carregar sessões:', err);
         }
 
     }
@@ -121,19 +119,19 @@ export default function VerTreinamento() {
             </div>
 
             {/* Corpo da página */}
-            <div className="row row-gap-3">
+            <div className='row row-gap-3'>
                 {/* Informações do treinamento */}
-                <div className="col-12 bg-white p-3 rounded shadow-sm d-flex flex-wrap row-gap-3">
-                    <div className="col-12 col-lg-6 pb-3 pb-lg-0 pe-md-3 d-flex flex-column gap-3 border-bottom">
+                <div className='col-12 bg-white p-3 rounded shadow-sm d-flex flex-wrap row-gap-3'>
+                    <div className='col-12 col-lg-6 pb-3 pb-lg-0 pe-md-3 d-flex flex-column gap-3 border-bottom'>
                         {/* Nome do treinamento */}
                         <div className='border-bottom border-2 '>
-                            <div className="fs-2">{treinamento?.nome ?? 'Nome do treinamento'}</div>
+                            <div className='fs-2'>{treinamento?.nome ?? 'Nome do treinamento'}</div>
                         </div>
 
                         {/* Datas */}
                         <div>
                             <div>Criado em {treinamento?.data_criacao ?? '00/00/0000'} por {treinamento?.criador ?? 'Criador'}</div>
-                            <div className="text-muted">Atualizado em {treinamento?.data_atualizacao ?? '00/00/0000'} por {treinamento?.criador ?? 'Criador'}</div>
+                            <div className='text-muted'>Atualizado em {treinamento?.data_atualizacao ?? '00/00/0000'} por {treinamento?.criador ?? 'Criador'}</div>
                         </div>
 
                         {/* Estado */}
@@ -148,35 +146,35 @@ export default function VerTreinamento() {
                         {/* Descrição */}
                         <div>
                             <div className='mb-0 fs-5'>Descrição</div>
-                            <div className="ms-3 p-2 rounded" style={{ minHeight: '250px' }}>
+                            <div className='ms-3 p-2 rounded' style={{ minHeight: '250px' }}>
                                 {treinamento?.descricao ?? 'Sem Descrição'}
                             </div>
                         </div>
                     </div>
 
-                    <div className="col-12 col-lg-6 pb-3 pb-lg-0 ps-md-3 d-flex flex-column gap-3 border-lg border-bottom border-start">
+                    <div className='col-12 col-lg-6 pb-3 pb-lg-0 ps-md-3 d-flex flex-column gap-3 border-lg border-bottom border-start'>
                         {/* Participantes */}
                         <button className='btn btn-White d-flex border d-flex align-items-center'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-arrow-right-short' viewBox='0 0 16 16'>
+                                <path fillRule='evenodd' d='M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8' />
                             </svg>
                             <div>Ver Participantes</div>
                         </button>
 
                         {/* Alterar dados */}
                         <button className='btn btn-White d-flex border '>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-arrow-right-short' viewBox='0 0 16 16'>
+                                <path fillRule='evenodd' d='M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8' />
                             </svg>
                             <div>Alterar informações</div>
                         </button>
                     </div>
 
-                    <div className="col-12 d-flex justify-content-between align-items-center">
+                    <div className='col-12 d-flex justify-content-between align-items-center'>
                         {/* Botão voltar e logo */}
                         <div>
-                            <a href="#" className="btn btn-azulGM">
-                                <i className="bi bi-arrow-left-short"></i> Voltar ao painel de controle
+                            <a href='#' className='btn btn-azulGM'>
+                                <i className='bi bi-arrow-left-short'></i> Voltar ao painel de controle
                             </a>
                         </div>
 
@@ -185,7 +183,7 @@ export default function VerTreinamento() {
                 </div>
 
                 {/* Sessões */}
-                <div className="col-12 bg-white p-3 rounded shadow-sm d-flex flex-wrap row-gap-3">
+                <div className='col-12 bg-white p-3 rounded shadow-sm d-flex flex-wrap row-gap-3'>
                     <Sessoes treinamento={treinamento}  sessoes={sessoes} registrarSessao={registrarSessao}/>
                 </div>
             </div>
