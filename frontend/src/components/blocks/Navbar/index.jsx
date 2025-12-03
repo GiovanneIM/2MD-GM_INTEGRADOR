5a31befe313c34e1648ec89810fc38abf923e3dd
Giovanne Isaac Marques <giovanneim020804@gmail.com>
2025-12-02 23:15:26 -0300

    logout

--- frontend/src/components/blocks/Navbar/index.jsx
+++ frontend/src/components/blocks/Navbar/index.jsx
@@ -12,6 +12,11 @@ export default function Navbar() {
 	const [usuario, setUsuario] = useState(null);
 	const [acesso, setAcesso] = useState(null);
 
+	function logout () {
+		sessionStorage.removeItem("token");
+		window.location.href = '/'
+	}
+
 	/* Carregando o usuário logado */
 	useEffect(() => {
 		try {
@@ -152,13 +157,13 @@ export default function Navbar() {
 			{/* Tema e Logo da GM */}
 			<div className='d-flex flex-lg-column'>
 				<div className='col-12 d-flex align-items-center justify-content-center mb-2'>
-					<a className='navbar__link rounded d-flex align-items-center justify-content-center'>
+					<button className='navbar__link rounded d-flex align-items-center justify-content-center' onClick={logout}>
 						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-door-open" viewBox="0 0 16 16">
 							<path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1" />
 							<path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117M11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5M4 1.934V15h6V1.077z" />
 						</svg>
 						<span className='rounded fw-bold d-none d-lg-block'>Sair</span>
-					</a>
+					</button>
 				</div>
 
 				<LogoGM tamanho={55} cor={'black'} />
